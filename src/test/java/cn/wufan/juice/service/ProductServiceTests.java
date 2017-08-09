d5a2b0a3751f6b579c8b5a189e191b730ae2e057
张斌 <zhangbin@skio.cn>
2017-08-09 11:00:43 +0800

    fix test

--- src/test/java/cn/wufan/juice/service/ProductServiceTests.java
+++ src/test/java/cn/wufan/juice/service/ProductServiceTests.java
@@ -1,20 +1,40 @@
 package cn.wufan.juice.service;
 
+import cn.wufan.juice.entity.Product;
+import cn.wufan.juice.mapper.ProductMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class ProductServiceTests {
+    @Configuration
+    static class ProductServiceTestContextConfiguration {
+        @Bean
+        public ProductService productService() {
+            return new ProductService();
+        }
+        @Bean
+        public ProductMapper productMapper() {
+            return Mockito.mock(ProductMapper.class);
+        }
+    }
 
     @Autowired
     private ProductService productService;
 
     @Test
     public void findAllTest(){
-
+        List<Product> hots = productService.hots();
+        Assert.isTrue(hots.size() == 0, "no hots");
     }
 }
